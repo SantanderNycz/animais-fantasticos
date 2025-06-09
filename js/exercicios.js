187723535289b289546b4f90424a09ec6f344442
Leo Santander <santandernycz.ls@gmail.com>
2025-06-09 16:01:14 +0100

    atualizacao - 45%

---
+++ js/exercicios.js
@@ -0,0 +1,8 @@
+// Divida o projeto em diferentes módulos
+"use strict";
+
+nome = "Ford"; // erro, variável global
+delete Array.prototype; // erro, não deletável
+window.top = 200; // erro, não pode mudar
+const arguments = 3.14; // escrever em palavra reservada
+
