735ae9d20bc08bafe6275d1c9f6040eb8b562737
Leo Santander <santandernycz.ls@gmail.com>
2025-11-07 00:19:23 +0000

    modal refatorado com class

--- js/modules/tabnav.js
+++ js/modules/tabnav.js
@@ -8,13 +8,13 @@ export default class TabNav {
   // Ativa a tab de acordo com o index da mesma
   activeTab(index) {
     this.tabContent.forEach((section) => {
-      section.classList.remove("ativo");
+      section.classList.remove(this.activeClass);
     });
     const direcao = this.tabContent[index].dataset.anime;
-    this.tabContent[index].classList.add("ativo", direcao);
+    this.tabContent[index].classList.add(this.activeClass, direcao);
   }
 
-  // adiciona os eventos nas tabs
+  // Adiciona os eventos nas tabs
   addTabNavEvent() {
     this.tabMenu.forEach((itemMenu, index) => {
       itemMenu.addEventListener("click", () => this.activeTab(index));
