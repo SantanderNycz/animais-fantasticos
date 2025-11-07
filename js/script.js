e8dd2099ff599d6dcfc0c15050bd7420bcc8b81e
Leo Santander <santandernycz.ls@gmail.com>
2025-11-07 18:06:21 +0000

    fix path

--- js/script.js
+++ js/script.js
@@ -43,6 +43,6 @@ menuMobile.init();
 
 initFuncionamento();
 
-fetchAnimais("../../animaisapi.json", ".numeros-grid");
+fetchAnimais("./animaisapi.json", ".numeros-grid");
 
 fetchBitcoin("https://blockchain.info/ticker", ".btc-preco");
